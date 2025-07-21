8fdef511868a1d65069d2f13574d893af23e5e75
Your Name <you@example.com>
2025-07-20 22:11:43 -0400

    Base commit

---
+++ server.js
@@ -0,0 +1,115 @@
+const HTTP_PORT = process.env.PORT || 8080;
+
+const express = require("express");
+const app = express();
+app.use(express.static("public"));  
+app.set("view engine", "ejs");      //ejs
+app.use(express.urlencoded({ extended: true })); //forms
+require("dotenv").config()   
+
+app.set('views', __dirname + '/views');
+require('pg');
+
+// +++ Database connection code
+// +++ TODO: Remember to add your Neon.tech connection variables to the .env file!!
+const { Sequelize } = require("sequelize")
+const sequelize = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
+  host: process.env.PGHOST,
+  dialect: "postgres",
+  port: 5432,
+  dialectOptions: {
+    ssl: { rejectUnauthorized: false },
+  },
+});
+
+// +++  4. TODO: Define your database table
+const Location = sequelize.define("Location",{
+name:Sequelize.TEXT,
+address:Sequelize.TEXT,
+category:Sequelize.TEXT,
+comments:Sequelize.TEXT,
+image:Sequelize.TEXT
+},
+{
+createdAt:false,
+updatedAt:false
+});
+
+
+
+// +++ 5. TODO: Define your server routes
+
+
+app.get('/',async(req,res)=>{
+    try{
+    const location = await Location.findAll();
+    return res.render("home.ejs",{location});
+    }catch(err)
+    {
+    console.log(err);
+    }
+});
+app.get("/memories/add", (req, res) => {    
+    return res.render("add.ejs")
+})
+
+
+app.post("/memories/add",async(req,res)=>{
+try
+{
+    await Location.create(
+    {
+        name:req.body.name,
+        address:req.body.address,
+        category:req.body.category,
+        comments:req.body.comments,
+        image:req.body.image
+
+    });
+res.redirect('/');//the same here to go back to the home page
+}catch(err)
+{
+console.log(err);
+res.redirect('/');//in order to go back to the  home page
+}
+
+});
+app.get("/memories/delete/:id",async(req,res)=>{
+try
+{
+    const id = req.params.id;
+    await Location.destroy({where:{id}});
+console.log("deleted rows");
+return res.redirect("/");
+}
+catch(err)
+{
+console.log(err);
+return res.redirect("/");
+}
+})
+// +++  Function to start serer
+async function startServer() {
+    
+    try {            
+        await sequelize.authenticate();        
+        await sequelize.sync()
+
+        console.log("SUCCESS connecting to database")
+        console.log("STARTING Express web server")        
+        
+        app.listen(HTTP_PORT, () => {     
+            console.log(`server listening on: http://localhost:${HTTP_PORT}`) 
+        })    
+    }    
+    catch (err) {        
+        console.log("ERROR: connecting to database")        
+        console.log(err)
+        console.log("Please resolve these errors and try again.")
+    }
+}
+
+startServer()
+
+
+
